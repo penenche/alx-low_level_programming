3edda3c20f816f5f9e21b0994356479b668dc02a
penenche <penenche27@gmail.com>
2022-04-07 20:52:50 -0700

    six

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 /**
- * main -prints the size of vatious types on the computer it is compiled and run on
+ * main -prints the size of vatious types
+ * on the computer it is compiled and run on
  * Return:Always 0 (Success)
  */
 int main(void)
 {
-	printf(size of a char:%d byte(s)\n",sizeof(char));
-	printf(size of an int:%d byte(s)\n",sizeof(int));
-	printf(size of a long int:%d byte(s)\n",sizeof(long int));
-	printf(size of a long long int:%d byte(s)\n",sizeof(long long int));
-	printf(size of a float:%d byte(s)\n",sizeof(float));
-	return (0)
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: %ld byte(s)\n", (long unsigned)sizeof(a));
+	printf("Size of an int: %ld byte(s)\n", (long unsigned)sizeof(b));
+	printf("Size of a long int: %ld byte(s)\n", (long unsigned)sizeof(c));
+	printf("Size of a long long int: %ld byte(s)\n", (long unsigned)sizeof(d));
+	printf("Size of a float: %ld byte(s)\n", (long unsigned)sizeof(f));
+	return (0);
 }
