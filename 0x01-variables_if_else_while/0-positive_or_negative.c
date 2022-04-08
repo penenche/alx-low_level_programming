86d5fcf5965e2074da3b08d727dae9b9f07af7cc
penenche <penenche27@gmail.com>
2022-04-08 11:55:50 -0700

    zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,6 +14,7 @@ int main(void)
 
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -23,4 +24,5 @@ int main(void)
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	return (0);
+
 }
