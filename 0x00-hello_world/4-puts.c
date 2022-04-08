391391bc10cd84d1cd403bf854779149a75ffa9b
penenche <penenche27@gmail.com>
2022-04-07 17:27:48 -0700

    four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #!/bin/bash
 #include <stdio.h>
-/*
+/**
  * main -prints the phrase "Programming is like building a multilingual puzzle"
  *eturn: o if exited properly, non-zero otherwise
  */
 int main(void)
 {
-	puts("\" "Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
