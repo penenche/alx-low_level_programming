0247c17c684c5c1e7824d9dc2eb8219460dff5ce
penenche <penenche27@gmail.com>
2022-04-07 17:39:39 -0700

    five

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,12 @@
 #!/bin/bash
+#include <stdio.h>
+
+/**
+ * main -prints the with proper grammar, but the outcome is a piece of art
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
