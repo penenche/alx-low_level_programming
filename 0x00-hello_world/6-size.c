8ff49936955ee7c9ec60cfb593adbcdbabf7e1c7
penenche <penenche27@gmail.com>
2022-04-07 19:26:44 -0700

    six

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main -prints the size of vatious types on the computer it is compiled and run on
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	printf(size of a char:%d byte(s)\n", size of(char));
+	printf(size of an int:%d byte(s)\n", size of(int));
+	printf(size of a long int:%d byte(s)\n", size of(long int));
+	printf(size of a long long int:%d byte(s)\n", size of(long long int));
+	printf(size of a float:%d byte(s)\n", size of(float));
+	return (0)
+}
