c86f5c91e880f23ae736c692ca62c8c8cd41d972
penenche <penenche27@gmail.com>
2022-04-09 05:16:24 -0700

    seven

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ * followed by a new line
+ * Return:Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
