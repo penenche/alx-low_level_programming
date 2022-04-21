f3219cc5fbb1e4d09d63778aef9599b938958cc8
penenche <penenche27@gmail.com>
2022-04-21 14:03:01 -0700

    four

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,12 +7,12 @@ void print_rev(char *s)
 {
 	int i, j, len;
 
-	i = 0
+	i = 0;
 
-		while (s[i] != '\0')
-		{
-			i++;
-		}
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 	len = i;
 	for (j = len - 1; j >= 0; j--)
 	{
