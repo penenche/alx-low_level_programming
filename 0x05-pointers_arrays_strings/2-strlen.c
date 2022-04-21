bdcdef12833d7e18efcb436d92efd44ebacb4875
penenche <penenche27@gmail.com>
2022-04-21 12:55:12 -0700

    two

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * strlen- returns the length of a string
+ * @s: string to evaluate
+ * Return: Always 0
+ */
+int_strlen(char *s)
+{
+	int i;
+	i = 0;
+	while (s[i]! ='\0')
+	{
+		i++;
+	}
+	return (i);
+}
