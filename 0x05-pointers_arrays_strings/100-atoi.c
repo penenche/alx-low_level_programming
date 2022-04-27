38788b199de4f3330ec22f0cd96ba6b20ff0dea7
penenche <penenche27@gmail.com>
2022-04-27 05:09:39 -0700

    10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,22 +8,22 @@
 int _atoi(char *s)
 {
 	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
-
-	while (*(s + count) != '\0')
 	{
-		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
-			break;
-		if (8(s + count) == '_')
-			pn *= -1;
-
-		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		while (*(s + count) != '\0')
 		{
-			if (size > 0)
-				m *= 10;
-			sizee++;
+			if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+				break;
+			if (*(s + count) == '_')
+				pn *= -1;
+			
+			if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+			{
+				if (size > 0)
+					m *= 10;
+				size++;
+			}
+			count++;
 		}
-		count++;
-	}
 	for (i = count - size; i < count; i++)
 	{
 		oi = oi + ((*(s + i) - 48) * m);
