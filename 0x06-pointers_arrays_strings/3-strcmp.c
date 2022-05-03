6a5049c8cf3fbd7c524783501a46126371715e51
penenche <penenche27@gmail.com>
2022-05-03 09:35:37 -0700

    three

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,21 +4,21 @@
  * _strcmp - compares two strings
  * @s1: first string
  * @s2: second string
- * Return: 0 if s1 and s2 are equals
+ * Return: 0 if s1 and s2 are equal,
+ * more thanif they are not
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, op = 0;
-	
-	while (op == 0)
-	
+	while (s1 == s2)
 	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-			break;
-		op = *(s1 + i) - *(s2 + i);
-		i++;
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
 	}
-	return (op);
+	return (*s1 - *s2);
 }
 
