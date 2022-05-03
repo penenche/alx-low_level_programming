b4abdc5a7587bb3fe5ed4540d8f3175175aa2472
penenche <penenche27@gmail.com>
2022-05-03 06:46:31 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,10 +7,10 @@
  * Return:Always 0 (Success)
  */
 
-int main(int argc__attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char*argv[])
 
 {
 	printf("%s\n", *argv);
-	
+
 	return (0);
 }
