4aeb738bc0cdf1b56060aa7c86a7a680596bacfb
penenche <penenche27@gmail.com>
2022-05-03 08:47:30 -0700

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,11 +12,11 @@
 int main(int argc, char *argv[])
 
 {
-	int i, j, sum = 0;
-	
+	int i, j, add = 0;
+
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] !='\0'; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!isdigit(argv[i][j]))
 			{
@@ -24,8 +24,8 @@ int main(int argc, char *argv[])
 				return (1);
 			}
 		}
-		sum += atoi(argv[i]);
+		add += atoi(argv[i]);
 	}
-	printf("%d\n", sum);
+	printf("%d\n", add);
 	return (0);
 }
