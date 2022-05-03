a2268210627a0afaae9eab8b2922bbb04cd049ab
penenche <penenche27@gmail.com>
2022-05-03 08:09:27 -0700

    5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,18 +13,28 @@
 int main(int argc, char *argv[])
 
 {
-	int num, j, result;
-	int coins[] = {23,10,5,2,1};
+	int num,j, result;
+
+	int coins[] = {22, 10, 5, 2, 1};
 
 	if (argc!= 2)
 	{
 		printf("error\n");
-		result = 0;
-		{
-			printf("0\n");
-			return (0);
-		}
-		for (j = 0; j < 5 && num >= 0; j++)
+		result = 1;
+	}
+
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
 		{
 			result++;
 			num -= coins[j];
