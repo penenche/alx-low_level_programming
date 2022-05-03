2418562ef2051b0bd665ee8e80cedf109f2e6a71
penenche <penenche27@gmail.com>
2022-05-03 06:29:06 -0700

    0

--- 0-whatsmyname.c
+++ 0-whatsmyname.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - prints its name, followed by a new line.
- *
- *   * @argc: number of command line arguments.
- *
- *    * @argv: array that contains the program command line arguments.
- *
- *     * Return: 0 - success.
- *
- *      */
+ * main - prints the name of the programme
+ * @argc: number of command line arguments
+ * @argv: array that contains the program command line arguments
+ * Return:Always 0 (Success)
+ */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc__attribute__((unused)), char *argv[])
 
 {
-
-		printf("%s\n", argv[0]);
-
-			return (0);
-
+	printf("%s\n", *argv);
+	
+	return (0);
 }
