fee4c1bc0858358dfd864a92f416e9700b3e4cfe
penenche <penenche27@gmail.com>
2022-05-03 08:23:45 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,27 +12,12 @@
 int main(int argc, char *argv[])
 
 {
-	int i, res = 1;
-	
 	if (argc != 3)
 	
 	{
 		printf("%s\n", "Error");
-
 		return (1);
-
 	}
-
-	else
-	
-	{
-		for (i = 1; i < argc; i++)
-		{
-			res *= atoi(argv[i]);
-
-		}
-		printf("%d\n", res);
-	}
-
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
