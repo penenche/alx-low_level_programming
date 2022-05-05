bcbc0a24d334ef325eb0352d16f8b281e7c45a6e
penenche <penenche27@gmail.com>
2022-05-05 12:40:08 -0700

    two

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,6 @@ char *str_concat(char *s1, char *s2)
 {
 	char *s3;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
-	
 	while (s1 && s1[len1])
 		len1++;
 	while (s2 && s2[len2])
@@ -23,7 +22,6 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	i = 0;
 	j = 0;
-	
 	if (s1)
 	{
 		while (i < len1)
