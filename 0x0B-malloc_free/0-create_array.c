ca01065a27f98b3443b41299a7442eeebef75b70
penenche <penenche27@gmail.com>
2022-05-05 12:00:06 -0700

    zero

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	p = (char *) malloc(sizeof(char) *size);
+	p = (char *) malloc(sizeof(char) * size);
 
 	if (p == NULL)
 		return (0);
