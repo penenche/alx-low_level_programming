d6831695899e433548428529322c79815b45634b
penenche <penenche27@gmail.com>
2022-05-05 12:33:51 -0700

    two

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,9 +17,11 @@ char *str_concat(char *s1, char *s2)
 		len1++;
 	while (s2 && s2[len2])
 		len2++;
+
 	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (s3 == NULL)
 		return (NULL);
+
 	 i = 0;
 	 j = 0;
 
@@ -31,6 +33,7 @@ char *str_concat(char *s1, char *s2)
 			 i++;
 		 }
 	 }
+
 	 if (s2)
 	 {
 		 while (i < (len1 + len2))
@@ -41,5 +44,6 @@ char *str_concat(char *s1, char *s2)
 		 }
 	 }
 	 s3[i] = '\0';
+
 	 return (s3);
 }
