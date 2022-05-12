20113bb2553b91bd2b49bb1ff6ccb65b5ad896f9
penenche <penenche27@gmail.com>
2022-05-12 14:44:27 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,8 +2,6 @@
 
 #define _DOG_H_
 
-
-
 /**
  * dog_t - Typedef for struct dog
  */
@@ -27,11 +25,8 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
-
 dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
 
 #endif
