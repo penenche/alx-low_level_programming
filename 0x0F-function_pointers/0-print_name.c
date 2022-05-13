346ad89ee69c61a7b5292364db1509bd23a9b584
penenche <penenche27@gmail.com>
2022-05-13 14:03:45 -0700

    zero

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 
 #include "function_pointers.h"
 
@@ -12,7 +12,7 @@
 void print_name(char *name, void (*f)(char *))
 
 {
-	if (name && f)
-		
-		f(name);
+	if (!name || !f)
+		return;
+	f(name);
 }
